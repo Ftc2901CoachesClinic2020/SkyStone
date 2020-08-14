1d6618ba3b1f01b0276b62daa68cb0c99599e10e
Mary Ellen Toebes <maryellen@toebes.com>
2020-08-13 22:34:02 -0400

    MrsToebes_Teleop - practicing

--- TeamCode/src/main/java/TeleOp/MrsToebes_Teleop.java
+++ TeamCode/src/main/java/TeleOp/MrsToebes_Teleop.java
@@ -50,7 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: MrsToebes TeleOp ", group="Linear Opmode")
+@TeleOp(name="Basic: MrsToebes TeleOp!", group="Linear Opmode")
 public class MrsToebes_Teleop extends LinearOpMode {
 
     // Declare OpMode members.
