603884b3f66a5afdc0fce26233e17ff994f28089
Margaret Toebes <margaret.toebes@gmail.com>
2020-08-13 22:35:09 -0400

    add spaces to MsToebes_Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MsToebes_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MsToebes_Teleop.java
@@ -53,6 +53,8 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="Basic: MsToebes Teleop", group="Linear Opmode")
 public class MsToebes_Teleop extends LinearOpMode {
 
+
+
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
